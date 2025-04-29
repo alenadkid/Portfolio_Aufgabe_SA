91b81cc3386f3704192702daa2ed0b340341d915
alenadkid <148689728+alenadkid@users.noreply.github.com>
2025-04-29 13:33:37 +0200

    Add files via upload

---
+++ src/test/java/com/SA/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SA.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
